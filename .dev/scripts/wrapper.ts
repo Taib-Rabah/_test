df4385fe1a8fae4e8c449aa62ecfdddcd070bf53
Taib Rabah <taib.rabah20@gmail.com>
2025-02-08 10:22:59 +0100

    fix: worfklows + wrapper script

--- .dev/scripts/wrapper.ts
+++ .dev/scripts/wrapper.ts
@@ -1,6 +1,6 @@
 import { joinPwd } from "./utils";
 
-const relativePath = process.argv.slice(2);
+const relativePath = process.argv.slice(3);
 
 const scriptPath = joinPwd(".dev/scripts", ...relativePath);
 
